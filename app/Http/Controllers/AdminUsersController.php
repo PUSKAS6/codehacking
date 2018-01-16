a36b84e663977c6a60a30f3bf2a6243945708208
vagrant <vagrant@laravelref>
2018-01-16 01:37:43 +0000

    users able to persist

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersRequest;
+use App\User;
+use App\Role;
 use Illuminate\Http\Request;
 
 class AdminUsersController extends Controller
@@ -13,7 +16,8 @@ class AdminUsersController extends Controller
      */
     public function index()
     {
-        return view('admin.users.index');
+        $users = User::all();
+        return view('admin.users.index', compact('users'));
         //
     }
 
@@ -24,8 +28,13 @@ class AdminUsersController extends Controller
      */
     public function create()
     {
-        return view('admin.users.create');
-        //
+        $roles = Role::pluck('name', 'id')->all();
+//        $roles = Role::lists('name', 'id')->all();
+        return view('admin.users.create', compact('roles'));
+
+
+
+
     }
 
     /**
@@ -34,10 +43,13 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
-        return view('admin.users.store');
-        //
+//        return view('admin.users.store');
+//        return $request->all();
+        User::create($request->all());
+        return redirect('/admin/users');
+//        //
     }
 
     /**
---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.admin')
+
+
+@section('content')
+    <h1>Create Users</h1>
+
+
+    <form action="{{ route('admin.users.store') }}" method="POST" enctype="multipart/form-data">
+        {{ csrf_field() }}
+        <div class="form-group">
+            <label for="name">Name:</label>
+            <input type="text" name="name" id="name" class="form-control">
+        </div>
+        <!-- ./.form-group -->
+
+
+
+        <div class="form-group">
+            <label for="email">E-mail:</label>
+            <input type="email" name="email" id="email"  class="form-control">
+        </div>
+        <!-- ./.form-group -->
+
+        <div class="form-group">
+            <label for="role_id">Role:</label>
+            <input type="text" name="role_id"  class="form-control">
+
+            {{--<select name="role_id" id="role_id">--}}
+        {{--@foreach($roles as $role)--}}
+        {{--<option value="{{ $role->id }}">{{ $role->name }}</option>--}}
+        {{--@endforeach--}}
+        {{--</select>--}}
+        </div>
+
+
+
+        <div class="form-group">
+            <label for="is_active">Is Active:</label>
+            <select name="is_active" id="is_active">
+                <option value="1">Active</option>
+                <option value="0">Not Active</option>
+            </select>
+        </div>
+        <!-- ./.form-group -->
+
+        <div class="form-group">
+            <label for="photo_id">Photo:</label>
+            <input type="file" name="photo_id" id="photo_id"  class="form-control">
+        </div>
+        <!-- ./.form-group -->
+
+        <div class="form-group">
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="password"  class="form-control">
+        </div>
+        <!-- ./.form-group -->
+
+        <div class="form-group">
+            <button class="btn btn-primary" type="submit" name="submit">Create Users</button>
+        </div>
+        <!-- ./.form-group -->
+
+    </form>
+
+@include('includes.form_error')
+
+
+@stop
